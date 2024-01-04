0903818538b89a2aea95f45b7a11080aa4081d8a
Ahmet Göktürk Kurt <ahmetgokturkkurt3@gmail.com>
2024-01-04 16:09:07 +0300

    GuessTheFlag
    GuessTheFlag  game is finish.

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -22,26 +22,18 @@ struct ContentView: View {
     var body: some View {
         VStack {
             ZStack {
-                LinearGradient(colors: [.blue, .white], startPoint: .top, endPoint: .bottom)
-                    .ignoresSafeArea()
-                VStack {
-                    Text("Tap the flag")
-                    Text("")
-                    ForEach(0..<3) { number in
-                        Button(action: {
-                            flagTapped(number)
-                        }, label: {
-                            Image(flags[number])
-                        })
-                        .padding()
-                    }
-                }
-            }.alert("", isPresented: $showingScore) {
+                RadialGradient(stops: [
+                    .init(color: Color(red: 0.15, green: 0.25, blue: 0.45), location: 0.3),
+                    .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)],
+                               center: .top, startRadius: 200, endRadius: 700)
+                .ignoresSafeArea()
+                flagView
+            }.alert("Important Message", isPresented: $showingScore) {
                 Button("Countinue") {
                     askQuestion()
                 }
             }message: {
-                Text("\(showingTitle), Your score: \(score) ")
+                Text("Your choose \(showingTitle)")
             }
         }
     }
@@ -50,17 +42,64 @@ struct ContentView: View {
         if number == correctAnswer {
             showingTitle = "Correct"
             score += 1
+            if score == 8 {
+                score = 0
+                showingTitle = "You win, restart game"
+            }
         }else {
-            showingTitle = "False"
+            showingTitle = "false, that is \(flags[number]) flag"
             score -= 1
         }
         showingScore = true
+        
+        
     }
+    
     
     func askQuestion() {
         flags.shuffle()
         correctAnswer = Int.random(in: 0...2)
-        
+    }
+    
+    var flagView : some View {
+        VStack {
+            Spacer()
+            Text("Guess The Flag")
+                .foregroundStyle(.white)
+                .font(.largeTitle.bold())
+            VStack {
+                Text("Tap the flag")
+                    .foregroundStyle(.secondary)
+                    .font(.subheadline.weight(.heavy))
+                
+                Text(flags[correctAnswer])
+                    
+                    .font(.largeTitle.weight(.semibold))
+                
+                ForEach(0..<3) { number in
+                    Button(action: {
+                        flagTapped(number)
+                    }, label: {
+                        Image(flags[number])
+                            .clipShape(.capsule)
+                            .shadow(radius: 10)
+                    })
+                    .padding()
+                }
+            }
+            .frame(maxWidth: .infinity)
+            .padding(.vertical ,20)
+            .background(.regularMaterial)
+            .clipShape(.rect(cornerRadius: 20))
+            
+            Spacer()
+            
+            Text("Your Score \(score) ")
+                .foregroundStyle(.white)
+                .font(.largeTitle.bold())
+            Spacer()
+            
+        }.padding()
     }
 }
 
